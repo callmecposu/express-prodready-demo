79cdbdbea7919c5e2ab27be1a452d1a8fac11014
callmecposu <pc.cposu@gmail.com>
2023-07-07 23:22:48 -0400

    will it work 1st time?

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('get /');
+    res.send('<h1>hello there</h1>');
+})
+
+app.get('/users', (req, res) => {
+    console.log('get /users')
+    res.json([
+        {
+            uname: 'callmecposu',
+            pword: '1234'
+        },
+        {
+            uname: 'jmanalo',
+            pword: '12345678'
+        }
+    ]);
+})
+
+app.listen(3001, () => {
+    console.log('now listening at port 3001');
+})
